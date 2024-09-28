f84bc2abfc0899411fbd600406887fb6c090bbcf
chitransh <chitranshshrivastava102@gmail.com>
2024-09-29 02:06:58 +0530

    update

--- src/Components/HomeProduct/index.js
+++ src/Components/HomeProduct/index.js
@@ -1,67 +1,72 @@
 import Button from '@mui/material/Button';
-import React from "react";
+import React from 'react';
 import ProductItem from '../ProductItem';
-import { IoIosArrowRoundForward } from "react-icons/io";
+import { IoIosArrowRoundForward } from 'react-icons/io';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-
 const HomeProduct = () => {
-    var productSlideroptions = {
+    const productSliderOptions = {
         dots: true,
         infinite: false,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
     };
+
     return (
-        <>
-            <section className="homeProducts">
-                
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-3">
-                            <div className="banner">
-                                <img 
-                                    src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg" 
-                                    alt=""
-                                    className="cursor w-100"
-                                />
+        <section className="homeProducts">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="banner">
+                            <img
+                                src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg"
+                                alt="Banner"
+                                className="cursor w-100"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-md-9 productRow">
+                        <div className="d-flex align-items-center">
+                            <div className="info w-75">
+                                <h3 className="mb-0 hd">BEST SELLER</h3>
+                                <p className="text-light text-sml mb-0">
+                                    Do not miss the current offers until the end of March.
+                                </p>
                             </div>
+
+                            <Button className="viewAllbtn ml-auto">
+                                View All <IoIosArrowRoundForward />
+                            </Button>
                         </div>
 
-                        <div className="col-md-9 productRow">
-                            <div className="d-flex align-items-center">
-                                <div className="info w-75">
-                                    <h3 className="mb-0 hd">BEST SELLER</h3>
-                                    <p className="text-light text-sml mb-0">Do not miss the current offers until the end of March.</p>
-                                </div>
+                        <div className="product_row w-100 mt-4">
+                            <ProductItem />
+                        </div>
 
-                                <Button className='viewAllbtn ml-auto'>View All<IoIosArrowRoundForward/></Button>
-                            </div>
-                            <div className='product_row w-100 mt-4'>
-                                <ProductItem/>
-                            </div>
-
-                            <div className="d-flex align-items-center mt-5">
-                                <div className="info w-75">
-                                    <h3 className="mb-0 hd">NEW PRODUCTS</h3>
-                                    <p className="text-light text-sml mb-0">New products with update stocks.</p>
-                                </div>
-
-                                <Button className='viewAllbtn ml-auto'>View All<IoIosArrowRoundForward/></Button>
+                        <div className="d-flex align-items-center mt-5">
+                            <div className="info w-75">
+                                <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                                <p className="text-light text-sml mb-0">
+                                    New products with updated stocks.
+                                </p>
                             </div>
 
-                            <div className='product_row w-100 mt-4'>
-                                <ProductItem/>
-                            </div>
+                            <Button className="viewAllbtn ml-auto">
+                                View All <IoIosArrowRoundForward />
+                            </Button>
+                        </div>
+
+                        <div className="product_row w-100 mt-4">
+                            <ProductItem />
                         </div>
                     </div>
                 </div>
-
-            </section>
-        </>
+            </div>
+        </section>
     );
-}
+};
 
-export default HomeProduct
+export default HomeProduct;
